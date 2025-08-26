a57a87f57c0f1343316b42034971482b4d3ac9d4
shreyagharat2003 <shreyagharat2003@gmail.com>
2025-08-26 09:44:42 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,10 @@
+#Using the Math Module for Calculations
+import math
+num = float(input("Enter a number: "))
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+print("Square root: ", square_root)
+print("Natural log: ", natural_log)
+print("Sine value: ", sine_value)
+
